65e3fa56092edb8698bb6d2cb7d6d17d40e1a2a0
Hridoy-Dhar <hrid323232@gmail.com>
2024-06-16 12:32:42 +0600

    Update age_calssify.dart

--- bin/Dart-Basic/Dart-condtions-loops/age_calssify.dart
+++ bin/Dart-Basic/Dart-condtions-loops/age_calssify.dart
@@ -1,1 +1,28 @@
 /*Classify a person's age group (child, teenager, adult, senior). */
+import 'dart:io';
+
+void main() {
+  String ageGroup(int age) {
+    if (age < 0) {
+      return 'Invalid age';
+    } else if (age <= 12) {
+      return 'Child';
+    } else if (age <= 19) {
+      return 'Teenager';
+    } else if (age <= 64) {
+      return 'Adult';
+    } else {
+      return 'Senior';
+    }
+  }
+
+  print('Enter the age:');
+  int? age = int.tryParse(stdin.readLineSync() ?? '');
+
+  if (age == null) {
+    print('Error: Invalid input.');
+  } else {
+    String ageg = ageGroup(age);
+    print('The age group is: $ageg');
+  }
+}
